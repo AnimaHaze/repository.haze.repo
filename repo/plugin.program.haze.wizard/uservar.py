4354f69bb90499b24e8166376068ae353c7cb1bb
AnimaHaze <clouddd70@gmail.com>
2025-01-02 21:23:16 +0100

    wizard commit

---
+++ repo/plugin.program.haze.wizard/uservar.py
@@ -0,0 +1,8 @@
+'''#####-----Build File-----#####'''
+buildfile = 'http://'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.program.haze.wizard,repository.haze']
