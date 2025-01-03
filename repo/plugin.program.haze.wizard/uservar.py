fdebdc5f021c568566ceb91409dad30a8e367811
AnimaHaze <clouddd70@gmail.com>
2025-01-03 02:44:09 +0100

    Updated

--- repo/plugin.program.haze.wizard/uservar.py
+++ repo/plugin.program.haze.wizard/uservar.py
@@ -1,8 +1,8 @@
 '''#####-----Build File-----#####'''
-buildfile = 'http://'
+buildfile = 'http://raw.githubusercontent.com/AnimaHaze/repository.haze.repo/main/repo/builds/builds.json'
 
 '''#####-----Notifications File-----#####'''
-notify_url  = 'http://'
+notify_url  = 'https://raw.githubusercontent.com/AnimaHaze/repository.haze.repo/main/repo/builds/notify.txt'
 
 '''#####-----Excludes-----#####'''
 excludes  = ['plugin.program.haze.wizard,repository.haze']
